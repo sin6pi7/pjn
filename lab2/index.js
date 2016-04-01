5cca773618f93270d994576e00b99052735b625d
jacek-zyla <jacek.zyla@schibsted.pl>
2016-04-01 18:55:26 +0200

    Iterative distance.

--- lab2/index.js
+++ lab2/index.js
@@ -1,14 +1,41 @@
 const readline = require('readline');
-const LD = require('./ld');
-const SEPARATOR = ' ';
+const fs = require('fs');
+const iconvlite = require('iconv-lite');
+
+const ld = require('./ld');
+
+const SEPARATOR = '\n';
+const PATH = `${__dirname}/data/forms.txt`;
+const ENCODING = 'ISO-8859-2';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question(`Type two words to compare (separatd with "${SEPARATOR}"):`, (answer) => {
-  const words = answer.split(SEPARATOR);
+rl.question(`Type a word: `, (answer) => {
   rl.close();
-  console.log(`Levenstein distnace between "${words[0]}" and "${words[1]}" is ${LD(words[0], words[1])}`);
+
+  console.log('Reading words into memory...');
+  const word = answer;
+  const text = fs.readFileSync(PATH);
+  const forms = iconvlite.decode(text, ENCODING).split(SEPARATOR);
+  console.log('Finished reading.\nCalculating distances...');
+
+  const start = Date.now();
+
+  const result = forms
+    .map(form => {
+      return {
+        distance: ld(form, word),
+        form: form
+      };
+    })
+    .reduce((res, cur) => res.distance < cur.distance ? res : cur, {
+      distance: Number.MAX_VALUE,
+      form: '404'
+    });
+
+  console.log('Finished calcultaing distances.');
+  console.log(`Closest: ${result.form} (LD: ${result.distance}, time: ${Date.now() - start}ms)`);
 });
--- lab2/ld.js
+++ lab2/ld.js
@@ -1,95 +1,109 @@
-const MISTAKES = {
-  DIACRITIC: {
-    'ą': 'a',
-    'ć': 'c',
-    'ę': 'e',
-    'ł': 'l',
-    'ń': 'n',
-    'ó': 'o',
-    'ś': 's',
-    'ź': 'z',
-    'ż': 'z'
-  },
-  MISSPELLINGS: {
-    'rz': 'ż',
-    'ó': 'u',
-    'ż': 'rz',
-    'h': 'ch',
-    'b': 'p',
-    'ji': 'i',
-    'ii': 'i',
-    'c': 'dz',
-    'sz': 'rz',
-    'ń': 'ni',
-    'ci': 'ć',
-    'dź': 'ć',
-    'ą': 'om',
-    'ś': 'si',
-  }
-};
+'use strict';
 
-const MISTAKE_VALUES = {
+const COST_VALUES = {
+  EQUAL: 0,
   DIACRITIC: 0.25,
   MISSPELLING: {
-    SINGLE: 0.5,
-    DOUBLE: -0.5
+    SINGLE_SINGLE: 0.5,
+    SINGLE_DOUBLE: -0.5,
+    DOUBLE_DOUBLE: 0.5
   },
-  CZECH: -0.5
+  CZECH: -0.5,
+  COMPLETELY_DIFFERENT: 1
+};
+
+const COSTS = {
+  // DIACRITIC
+  'ąa': COST_VALUES.DIACRITIC,
+  'aą': COST_VALUES.DIACRITIC,
+  'ćc': COST_VALUES.DIACRITIC,
+  'cć': COST_VALUES.DIACRITIC,
+  'eę': COST_VALUES.DIACRITIC,
+  'ęe': COST_VALUES.DIACRITIC,
+  'lł': COST_VALUES.DIACRITIC,
+  'łl': COST_VALUES.DIACRITIC,
+  'nń': COST_VALUES.DIACRITIC,
+  'ńn': COST_VALUES.DIACRITIC,
+  'oó': COST_VALUES.DIACRITIC,
+  'óo': COST_VALUES.DIACRITIC,
+  'śs': COST_VALUES.DIACRITIC,
+  'sś': COST_VALUES.DIACRITIC,
+  'źz': COST_VALUES.DIACRITIC,
+  'zź': COST_VALUES.DIACRITIC,
+  'zż': COST_VALUES.DIACRITIC,
+  'żz': COST_VALUES.DIACRITIC,
+
+  // MISSPELLING
+  // single - single
+  'óu': COST_VALUES.MISSPELLING.SINGLE_SINGLE,
+  'uó': COST_VALUES.MISSPELLING.SINGLE_SINGLE,
+  'bp': COST_VALUES.MISSPELLING.SINGLE_SINGLE,
+  'pb': COST_VALUES.MISSPELLING.SINGLE_SINGLE,
+  // single - double
+  'rzż': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'żrz': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'hch': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'chh': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'jii': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'iji': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'iii': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'cdz': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'dzc': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'ńni': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'niń': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'cić': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'ćci': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'dźć': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'ćdź': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'ąom': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'omą': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'śsi': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  'siś': COST_VALUES.MISSPELLING.SINGLE_DOUBLE,
+  // double - double
+  'szrz': COST_VALUES.MISSPELLING.DOUBLE_DOUBLE,
+  'rzsz': COST_VALUES.MISSPELLING.DOUBLE_DOUBLE
 };
 
 module.exports = (w1, w2) => {
-  return lev(w1, w2, w1.length, w2.length);
+  return lev(w1, w1.length, w2, w2.length);
 };
 
-function lev(w1, w2, l1, l2) {
-  if (Math.min(l1, l2) === 0) {
-    return Math.max(l1, l2);
+function lev(w1, n1, w2, n2) {
+  const distances = [[0]];
+
+  for (let i = 1; i <= n1; ++i) {
+    distances[i] = [i];
   }
 
-  return Math.min(
-    lev(w1, w2, l1 - 1, l2) + 1,
-    lev(w1, w2, l1, l2 - 1) + 1,
-    lev(w1, w2, l1 - 1, l2 - 1) + signDistance(w1, l1 - 1, w2, l2 - 1)
-  );
+  for (let i = 1; i <= n2; ++i) {
+    distances[0][i] = i;
+  }
+
+  for (let i = 1; i <= n1; ++i) {
+    for (let j = 1; j <= n2; ++j) {
+      distances[i][j] = Math.min(
+        distances[i - 1][j] + 1,
+        distances[i][j - 1] + 1,
+        distances[i - 1][j - 1] + signsDistance(w1, i - 1, w2, j - 1)
+      );
+    }
+  }
+
+  return distances[n1][n2];
 }
 
-function signDistance(w1, i1, w2, i2) {
+function signsDistance(w1, i1, w2, i2) {
   const s11 = w1[i1];
   const s12 = w1[i1 + 1];
   const s21 = w2[i2];
   const s22 = w2[i2 + 1];
 
-  if (s11 === s21) {
-    return 0;
-  }
-
-  if (
-    MISTAKES.DIACRITIC[s11] === s21 ||
-    MISTAKES.DIACRITIC[s21] === s11
-  ) {
-    return MISTAKE_VALUES.DIACRITIC;
-  }
-
-  if (
-    MISTAKES.MISSPELLINGS[s11] === s21 ||
-    MISTAKES.MISSPELLINGS[s21] === s11
-  ) {
-    return MISTAKE_VALUES.MISSPELLING.SINGLE;
-  }
-
-  if (
-    MISTAKES.MISSPELLINGS[`${s11}${s12}`] === s21 ||
-    MISTAKES.MISSPELLINGS[`${s21}${s22}`] === s11
-  ) {
-    return MISTAKE_VALUES.MISSPELLING.DOUBLE;
-  }
-
-  if (
-    s11 === s22 &&
-    s12 === s21
-  ) {
-    return MISTAKE_VALUES.CZECH;
-  }
-
-  return 1;
+  return (s11 === s21) ? COST_VALUES.EQUAL : (
+    COSTS[`${s11}${s21}`] ||
+    COSTS[`${s11}${s21}${s22}`] ||
+    COSTS[`${s21}${s11}${s12}`] ||
+    COSTS[`${s21}${s22}${s11}${s12}`] ||
+    ((s11 === s22 && s21 === s12) && COST_VALUES.CZECH) ||
+    COST_VALUES.COMPLETELY_DIFFERENT
+  );
 }
